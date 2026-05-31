4276aaa84e543ce68e514f54183d13d0ad215451
ZzGreekzZ <161692516+QuocAnh242@users.noreply.github.com>
2026-05-31 19:20:24 +0700

    (feat): Implement Security through Api-gateway

--- profile-service/src/main/java/com/devteria/profile/configuration/CustomJwtDecoder.java
+++ profile-service/src/main/java/com/devteria/profile/configuration/CustomJwtDecoder.java
@@ -1,30 +1,18 @@
-package com.devteria.identity.configuration;
+package com.devteria.profile.configuration;
 
 import java.text.ParseException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.stereotype.Component;
 
-import com.devteria.identity.service.AuthenticationService;
 import com.nimbusds.jwt.SignedJWT;
 
 @Component
 public class CustomJwtDecoder implements JwtDecoder {
-    @Value("${jwt.signerKey}")
-    private String signerKey;
 
-    private final AuthenticationService authenticationService;
-
-    private NimbusJwtDecoder nimbusJwtDecoder = null;
-
-    public CustomJwtDecoder(AuthenticationService authenticationService) {
-        this.authenticationService = authenticationService;
-    }
-
+    // Read JWT Token
     @Override
     public Jwt decode(String token) throws JwtException {
         try {
