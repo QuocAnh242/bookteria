201878f379c166cf05b332c7fa8b60564f4d921f
ZzGreekzZ <161692516+QuocAnh242@users.noreply.github.com>
2026-06-01 10:48:10 +0700

    (feat): Implement email SMTP service

---
+++ notification-service/src/main/java/com/devteria/notification/service/EmailService.java
@@ -0,0 +1,42 @@
+package com.devteria.notification.service;
+
+import com.devteria.notification.dto.request.EmailRequest;
+import com.devteria.notification.dto.request.SendEmailRequest;
+import com.devteria.notification.dto.request.Sender;
+import com.devteria.notification.dto.response.EmailResponse;
+import com.devteria.notification.exception.AppException;
+import com.devteria.notification.exception.ErrorCode;
+import com.devteria.notification.repository.httpclient.EmailClient;
+import feign.FeignException;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class EmailService {
+    EmailClient emailClient;
+
+    String apiKey  = "xxxx";
+
+    public EmailResponse sendEmail(SendEmailRequest sendEmailRequest) {
+        EmailRequest emailRequest = EmailRequest.builder()
+                .sender(Sender.builder()
+                        .name("Devteria Dotcom")
+                        .email("wasasi241224@gmail.com")
+                        .build())
+                .to(List.of(sendEmailRequest.getTo()))
+                .subject(sendEmailRequest.getSubject())
+                .htmlContent(sendEmailRequest.getHtmlContent())
+                .build();
+        try {
+            return emailClient.sendEmail(apiKey, emailRequest);
+        } catch (FeignException e) {
+            throw new AppException(ErrorCode.CANNOT_SEND_EMAIL);
+        }
+    }
+}
